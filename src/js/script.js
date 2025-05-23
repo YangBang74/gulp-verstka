887c2c6c723a127f9518f1ee784e1363fbf2b86a
Yan <crowbeat.47@gmail.com>
2025-05-23 23:33:19 +0500

    feat: added send menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,23 @@
-  const swiper = new Swiper('.swiper', {
-    loop: true,
-    effect: 'fade',
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+const sendMenu = document.querySelector('.sub');
+
+const toggleSend = () => {
+  sendMenu.classList.toggle('active');
+  document.body.style.overflow = 'hidden';
+}
+const closeSendMenu = () => {
+  sendMenu.classList.remove('active');
+  document.body.style.overflow = 'auto';
+}  
