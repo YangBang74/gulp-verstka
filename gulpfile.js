5fcfbc8643b6ba1c782fa83af029a37e64aa3f8e
Yan <crowbeat.47@gmail.com>
2025-05-23 19:28:58 +0500

    fix: fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,75 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
-const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync').create();
-const prettier = require('gulp-prettier').default;
 
 // Пути
 const paths = {
-  html: 'src/*.html',
-  scss: 'src/scss/**/*.scss',
-  js: 'src/js/**/*.js',
-  images: 'src/images/**/*.{jpg,jpeg,png,svg,gif,webp}',
-  fonts: 'src/fonts/**/*.{woff,woff2,ttf,otf}',
-  dist: 'dist'
+  html: {
+    src: 'src/*.html',
+    dest: 'dist/'
+  },
+  styles: {
+    src: 'src/scss/**/*.scss',
+    dest: 'dist/css/'
+  },
+  scripts: {
+    src: 'src/js/**/*.js',
+    dest: 'dist/js/'
+  }
 };
 
 // HTML
 function html() {
-  return src(paths.html)
-    .pipe(dest(paths.dist))
+  return src(paths.html.src)
+    .pipe(dest(paths.html.dest))
     .pipe(browserSync.stream());
 }
 
-// SCSS
+// SCSS -> CSS
 function styles() {
-  return src(paths.scss)
+  return src(paths.styles.src)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({ cascade: false }))
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write('.'))
-    .pipe(dest(paths.dist + '/css'))
+    .pipe(dest(paths.styles.dest))
     .pipe(browserSync.stream());
 }
 
 // JS
 function scripts() {
-  return src(paths.js)
-    .pipe(sourcemaps.init())
-    .pipe(concat('main.js'))
+  return src(paths.scripts.src)
+    .pipe(concat('main.min.js'))
     .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(dest(paths.dist + '/js'))
+    .pipe(dest(paths.scripts.dest))
     .pipe(browserSync.stream());
 }
 
-// Images
-function images() {
-  return src(paths.images, { encoding: false })
-    .pipe(dest(paths.dist + '/images'))
-    .pipe(browserSync.stream());
-}
-
-// Fonts
-function fonts() {
-  return src(paths.fonts, { encoding: false })
-    .pipe(dest(paths.dist + '/fonts'))
-    .pipe(browserSync.stream());
-}
-
-// Format with Prettier
-function format() {
-  return src(['src/**/*.js', 'src/**/*.scss', 'src/**/*.html'], { base: './' })
-    .pipe(prettier())
-    .pipe(dest('./'));
-}
-
-// Watch + Live Reload
+// Watch
 function serve() {
   browserSync.init({
     server: {
-      baseDir: paths.dist
-    }
+      baseDir: 'dist/'
+    },
+    notify: false
   });
 
-  watch(paths.html, html);
-  watch(paths.scss, styles);
-  watch(paths.js, scripts);
-  watch(paths.images, images);
-  watch(paths.fonts, fonts);
+  watch(paths.html.src, html);
+  watch(paths.styles.src, styles);
+  watch(paths.scripts.src, scripts);
 }
 
 // Экспорт задач
-exports.default = series(
-  format,
-  parallel(html, styles, scripts, images, fonts),
-  serve
-);
+exports.html = html;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.serve = series(parallel(html, styles, scripts), serve);
+exports.default = exports.serve;
