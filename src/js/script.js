9f11a9315ffbe5020213d708cb2aa56e1bc6f0c8
Yan <crowbeat.47@gmail.com>
2025-05-24 13:43:00 +0500

    fix: send form

--- src/js/script.js
+++ src/js/script.js
@@ -21,3 +21,35 @@ const closeSendMenu = () => {
   sendMenu.classList.remove('active');
   document.body.style.overflow = 'auto';
 }  
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('contactForm');
+  const modal = document.getElementById('successModal');
+  const closeBtn = document.querySelectorAll('.closeModal')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();       // отменяем реальную отправку
+
+    // Здесь можешь вызвать fetch()/XHR для реальной отправки
+    // fetch('/api/send', { method: 'POST', body: new FormData(form) })
+    //   .then(() => { ... });
+
+    // Показать модалку
+    modal.classList.add('active');
+
+    // Очистить форму (если нужно)
+    form.reset();
+  });
+
+  // Закрыть по кнопке
+  closeBtn.forEach(btn => { btn.addEventListener('click', () => {
+      modal.classList.remove('active');
+    });
+  });
+  // Закрыть кликом вне контента
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.classList.remove('active');
+    }
+  });
+});
